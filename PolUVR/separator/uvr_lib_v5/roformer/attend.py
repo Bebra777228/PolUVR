3232ec3d98706d6566a693d8349c50ef720425eb
Politrees <143968312+Bebra777228@users.noreply.github.com>
2025-01-14 19:24:51 +0500

    Update attend.py

--- PolUVR/separator/uvr_lib_v5/roformer/attend.py
+++ PolUVR/separator/uvr_lib_v5/roformer/attend.py
@@ -3,25 +3,23 @@ from packaging import version
 from collections import namedtuple
 
 import torch
-from torch import nn, einsum
 import torch.nn.functional as F
+from torch import nn, einsum
 
 from einops import rearrange, reduce
 
 # constants
-
 FlashAttentionConfig = namedtuple("FlashAttentionConfig", ["enable_flash", "enable_math", "enable_mem_efficient"])
 
 # helpers
-
-
 def exists(val):
     return val is not None
 
+def default(v, d):
+    return v if exists(v) else d
 
 def once(fn):
     called = False
-
     @wraps(fn)
     def inner(x):
         nonlocal called
@@ -29,18 +27,15 @@ def once(fn):
             return
         called = True
         return fn(x)
-
     return inner
-
 
 print_once = once(print)
 
 # main class
-
-
 class Attend(nn.Module):
-    def __init__(self, dropout=0.0, flash=False):
+    def __init__(self, dropout=0.0, flash=False, scale=None):
         super().__init__()
+        self.scale = scale
         self.dropout = dropout
         self.attn_dropout = nn.Dropout(dropout)
 
@@ -48,7 +43,6 @@ class Attend(nn.Module):
         assert not (flash and version.parse(torch.__version__) < version.parse("2.0.0")), "in order to use flash attention, you must be using pytorch 2.0 or above"
 
         # determine efficient attention configs for cuda and cpu
-
         self.cpu_config = FlashAttentionConfig(True, True, True)
         self.cuda_config = None
 
@@ -61,13 +55,17 @@ class Attend(nn.Module):
             print_once("A100 GPU detected, using flash attention if input tensor is on cuda")
             self.cuda_config = FlashAttentionConfig(True, False, False)
         else:
+            print_once("Non-A100 GPU detected, using math or mem efficient attention if input tensor is on cuda")
             self.cuda_config = FlashAttentionConfig(False, True, True)
 
     def flash_attn(self, q, k, v):
         _, heads, q_len, _, k_len, is_cuda, device = *q.shape, k.shape[-2], q.is_cuda, q.device
 
-        # Check if there is a compatible device for flash attention
+        if exists(self.scale):
+            default_scale = q.shape[-1] ** -0.5
+            q = q * (self.scale / default_scale)
 
+        # Check if there is a compatible device for flash attention
         config = self.cuda_config if is_cuda else self.cpu_config
 
         # sdpa_flash kernel only supports float16 on sm80+ architecture gpu
@@ -91,22 +89,19 @@ class Attend(nn.Module):
 
         q_len, k_len, device = q.shape[-2], k.shape[-2], q.device
 
-        scale = q.shape[-1] ** -0.5
+        scale = default(self.scale, q.shape[-1] ** -0.5)
 
         if self.flash:
             return self.flash_attn(q, k, v)
 
         # similarity
-
         sim = einsum(f"b h i d, b h j d -> b h i j", q, k) * scale
 
         # attention
-
         attn = sim.softmax(dim=-1)
         attn = self.attn_dropout(attn)
 
         # aggregate values
-
         out = einsum(f"b h i j, b h j d -> b h i d", attn, v)
 
         return out
